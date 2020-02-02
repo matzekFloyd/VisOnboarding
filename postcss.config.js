8f4d9efc10f1e42996f44fa77fe23dbfa702de7c
Mathias Mayrhofer <it181508@fhstp.ac.at>
2020-02-02 22:19:06 +0100

    add purgecss config for tailwind.scss

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,23 @@
-// postcss.config.js
-const tailwindcss = require('tailwindcss');
+const purgecss = require('@fullhuman/postcss-purgecss')({
+    content: [
+        './src/**/*.jsx',
+        './src/**/*.js',
+        './public/index.html',
+        './pages/**/*.js',
+        './pages/onboarding/**/*.js'
+    ],
+    whitelist: ['html', 'body'],
+    css: ['./src/tailwind.scss'],
+    defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+});
+const cssnano = require('cssnano')({
+    preset: 'default',
+});
 
 module.exports = {
     plugins: [
-        tailwindcss('./tailwind.config.js'),
-        require('postcss-preset-env'),
-    ]
+        require('tailwindcss'),
+        require('autoprefixer'),
+        ...process.env.NODE_ENV === 'production' ? [purgecss, cssnano] : []
+    ],
 };
