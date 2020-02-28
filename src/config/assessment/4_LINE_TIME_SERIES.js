c252a7ffe40e48987af795f9adac4bd94e783621
Mathias Mayrhofer <it181508@fhstp.ac.at>
2020-02-28 16:27:57 +0100

    adds subquestion config for LINE_TIME_SERIES task

--- src/config/assessment/4_LINE_TIME_SERIES.js
+++ src/config/assessment/4_LINE_TIME_SERIES.js
@@ -9,5 +9,14 @@ export const LINE_TIME_SERIES = {
         {text: "The highest exchange rate was 0.988.", correct: false},
         {text: "The exchange rate has fallen between 2013 and 2018.", correct: false},
         {text: "On average the exchange rate is lower than 0.9.", correct: true},
-    ]
+    ],
+    subQuestion: {
+        question: "<strong>Select all applicable statements</strong>:",
+        options: [
+            {text: "The lowest point of the exchange rate was reached on March 25th 2011.", correct: false},
+            {text: "The highest point of the exchange rate was reached on December 20th 2016.", correct: true},
+            {text: "The highest point of the exchange rate was reached on March 13th 2015.", correct: false},
+            {text: "The lowest point of the exchange rate was reached on July 15th 2008.", correct: true}
+        ]
+    }
 };
