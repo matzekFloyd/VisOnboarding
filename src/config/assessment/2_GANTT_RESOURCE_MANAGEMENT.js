d3bb27a97917e5f9b14479388a64334ca100dd44
Mathias Mayrhofer <it181508@fhstp.ac.at>
2020-02-28 16:02:19 +0100

    adds subquestion config for GANTT_RESOURCE_MANAGEMENT task

--- src/config/assessment/2_GANTT_RESOURCE_MANAGEMENT.js
+++ src/config/assessment/2_GANTT_RESOURCE_MANAGEMENT.js
@@ -5,9 +5,19 @@ export const GANTT_RESOURCE_MANAGEMENT = {
     image: {id: "img-car-rental-schedule", src: "car-rental-schedule.png"},
     question: "<strong>Which assumptions can be made from reading this chart</strong>:",
     options: [
-        {text: "A car of the type <i>Nissan leaf</i> is available for renting on february 26th.", correct: true},
+        {text: "A car of the type <i>Nissan leaf</i> is available for renting on March 2nd.", correct: true},
         {text: "The most popular car model is the <i>Volvo V60</i>.", correct: false},
         {text: "Cars of type <i>Peugot 208</i> are rented for the longest time among all models.", correct: false},
-        {text: "On February 19 all cars were rented.", correct: true},
-    ]
+        {text: "On February 25th all cars were rented.", correct: true},
+    ],
+    subQuestion: {
+        question: "<strong>Which assumptions can be made from reading this chart</strong>:",
+        options: [
+            {text: "The least popular car model is the <i>Jaguar E-type</i>.", correct: true},
+            {
+                text: "On February 29th <i>Nissan Leaf</i>, as well as <i>Peugot 208</i> are available for renting.",
+                correct: true
+            },
+        ]
+    }
 };
