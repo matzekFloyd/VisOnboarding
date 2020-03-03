b91da31854dfea7ddcd54610eda4f768a742cc34
Mathias Mayrhofer <it181508@fhstp.ac.at>
2020-03-03 16:03:12 +0100

    adds content for proficient onboarding

---
+++ src/config/onboarding/proficient/charts/1_STEP.js
@@ -0,0 +1,87 @@
+import {convertUnixToString} from "src/util/helpers";
+
+let today = new Date(),
+    day = 1000 * 60 * 60 * 24;
+today.setUTCHours(0);
+today.setUTCMinutes(0);
+today.setUTCSeconds(0);
+today.setUTCMilliseconds(0);
+today = today.getTime();
+
+export const STEP_1 = {
+    chart: {
+        height: 600
+    },
+    tooltip: {
+        enabled: true,
+        formatter: function () {
+            let point = this.point;
+            let str = '<b> ' + point.name + '</b><br />';
+            if (point.start && point.milestone) {
+                str += convertUnixToString(point.start, true, true, true, false, false) + '<br />';
+            } else {
+                str += 'Start: ' + convertUnixToString(point.start, true, true, true, false, false) + '<br />';
+            }
+            if (point.end) str += 'End: ' + convertUnixToString(point.end, true, true, true, false, false) + '<br />';
+            if (point.duration) str += 'Duration: ' + point.duration + '<br />';
+            if (point.dependency) str += 'Dependency: ' + point.dependency + '<br />';
+            return str;
+        },
+        padding: 12,
+        shape: "square"
+    },
+    series: [{
+        name: 'Product',
+        data: [{
+            name: 'New product launch',
+            id: 'new_product',
+        }, {
+            name: 'Development',
+            id: 'development',
+            parent: 'new_product',
+            start: today - day,
+            end: today + (11 * day),
+            duration: 12 + " days",
+            completed: {
+                amount: 0.6,
+                fill: '#e80'
+            },
+        }, {
+            name: 'Beta',
+            id: 'beta',
+            dependency: 'development',
+            parent: 'new_product',
+            start: today + 12.5 * day,
+            milestone: true,
+        }, {
+            name: 'Final development',
+            id: 'finalize',
+            dependency: 'beta',
+            parent: 'new_product',
+            start: today + 13 * day,
+            end: today + 17 * day,
+            duration: 4 + " days"
+        }, {
+            name: 'Launch',
+            dependency: 'finalize',
+            parent: 'new_product',
+            start: today + 17.5 * day,
+            milestone: true,
+        }]
+    }],
+    colors: ['#621100', '#cf7611', '#ffba46', '#f4777f', '#93003a'],
+    title: {
+        text: 'Gantt Project Management'
+    },
+    xAxis: {
+        currentDateIndicator: true,
+        min: today - 3 * day,
+        max: today + 18 * day
+    },
+    exporting: {
+        enabled: false
+    },
+    credits: {
+        enabled: false
+    }
+};
---
+++ src/config/onboarding/proficient/charts/2_STEP.js
@@ -0,0 +1,117 @@
+import {convertUnixToString} from "src/util/helpers";
+
+let today = new Date();
+today.setUTCHours(0);
+today.setUTCMinutes(0);
+today.setUTCSeconds(0);
+today.setUTCMilliseconds(0);
+today = today.getTime();
+
+function getDuration(start, end) {
+    const diffTime = Math.abs(end - start);
+    return Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+}
+
+export const STEP_2 = {
+    chart: {
+        height: 600
+    },
+    tooltip: {
+        enabled: true,
+        formatter: function () {
+            let point = this.point;
+            let duration = getDuration(point.start, point.end);
+            let str = '<b> ' + point.name + '</b><br />';
+            if (point.start && point.milestone) {
+                str += convertUnixToString(point.start, true, true, true, false, false) + '<br />';
+            } else {
+                str += 'Start: ' + convertUnixToString(point.start, true, true, true, false, false) + '<br />';
+            }
+            if (point.end) str += 'End: ' + convertUnixToString(point.end, true, true, true, false, false) + '<br />';
+            if (duration) str += 'Duration: ' + duration + ' days<br />';
+            return str;
+        },
+        padding: 12,
+        shape: "square"
+    },
+    title: {
+        text: 'Gantt Project Management'
+    },
+    yAxis: {
+        uniqueNames: true
+    },
+    navigator: {
+        enabled: true,
+        liveRedraw: true,
+        series: {
+            type: 'gantt',
+            pointPlacement: 0.5,
+            pointPadding: 0.25
+        },
+        yAxis: {
+            min: 0,
+            max: 3,
+            reversed: true,
+            categories: []
+        }
+    },
+    scrollbar: {
+        enabled: true
+    },
+    rangeSelector: {
+        enabled: true,
+        selected: 0
+    },
+    series: [{
+        name: 'Product',
+        data: [{
+            name: 'New product launch',
+            id: 'new_product',
+        }, {
+            start: Date.UTC(2017, 11, 1),
+            end: Date.UTC(2018, 1, 2),
+            parent: 'new_product',
+            completed: 0.95,
+            name: 'Prototyping'
+        }, {
+            start: Date.UTC(2018, 1, 2),
+            end: Date.UTC(2018, 11, 5),
+            parent: 'new_product',
+            completed: 0.5,
+            name: 'Development',
+        }, {
+            start: Date.UTC(2018, 11, 6),
+            end: Date.UTC(2018, 11, 9),
+            parent: 'new_product',
+            completed: 0.15,
+            name: 'Testing',
+        }, {
+            start: Date.UTC(2018, 11, 9),
+            end: Date.UTC(2018, 11, 19),
+            parent: 'new_product',
+            completed: {
+                amount: 0.3,
+                fill: '#fa0'
+            },
+            name: 'Development'
+        }, {
+            start: Date.UTC(2018, 11, 10),
+            end: Date.UTC(2018, 11, 23),
+            parent: 'new_product',
+            name: 'Testing'
+        }, {
+            start: Date.UTC(2018, 11, 25, 8),
+            end: Date.UTC(2018, 11, 25, 16),
+            parent: 'new_product',
+            name: 'Release',
+            milestone: true
+        }],
+    }],
+    colors: ['#621100', '#cf7611', '#ffba46', '#f4777f', '#93003a'],
+    exporting: {
+        enabled: false
+    },
+    credits: {
+        enabled: false
+    }
+};
