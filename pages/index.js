988eca468210b8f684ce740d56073c9d3212437b
Mathias Mayrhofer <it181508@fhstp.ac.at>
2020-03-04 11:49:25 +0100

    add username input field to home screen

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,8 @@ class Home extends PureComponent {
         super(props, context);
         this.state = {
             loading: true,
-            mode: null
+            mode: null,
+            user: null
         };
     }
 
@@ -33,29 +34,50 @@ class Home extends PureComponent {
     }
 
     navigate(event, to) {
+        if (!this.validateInput()) return;
+
         event.preventDefault();
         Router.push(to).then(() => console.log("Redirecting: ", to));
     }
 
     initCards() {
         let cards = [];
+        let enabled = this.validateInput();
         if (this.activeMode(OPTIONAL)) {
-            cards.push(<OnboardingCard key={"card_1"} className={"mr-6 ml-auto"} onClick={(e) => this.navigate(e, URL.assessment)}/>);
-            cards.push(<TaskCard key={"card_2"} className={"ml-6 mr-auto"} onClick={(e) => this.navigate(e, URL.context)}/>)
+            cards.push(<OnboardingCard key={"card_1"} className={"mr-6 ml-auto"} enabled={enabled}
+                                       onClick={(e) => this.navigate(e, URL.assessment)}/>);
+            cards.push(<TaskCard key={"card_2"} className={"ml-6 mr-auto"} enabled={enabled}
+                                 onClick={(e) => this.navigate(e, URL.context)}/>)
         } else if (this.activeMode(ONBOARDING)) {
-            cards.push(<OnboardingCard className={"m-auto"} onClick={(e) => this.navigate(e, URL.assessment)}/>);
+            cards.push(<OnboardingCard className={"m-auto"} onClick={(e) => this.navigate(e, URL.assessment)}
+                                       enabled={enabled}/>);
         } else if (this.activeMode(TASK)) {
-            cards.push(<TaskCard className={"m-auto"} onClick={(e) => this.navigate(e, URL.context)}/>);
+            cards.push(<TaskCard className={"m-auto"} onClick={(e) => this.navigate(e, URL.context)}
+                                 enabled={enabled}/>);
         }
         return cards;
+    }
+
+    handleInputChange(e) {
+        this.setState({user: e.target.value});
+    }
+
+    validateInput() {
+        //TODO
+        return this.state.user ? true : false;
     }
 
     render() {
         return (
             <Layout>
                 {this.state.loading ? <Empty/> :
-                    <div className={"flex flex-wrap w-full h-screen"}>
-                        {this.initCards()}
+                    <div className={"mt-32 w-full"}>
+                        <div className={"flex w-full"}>
+                            {this.initCards()}
+                        </div>
+                        <div className={"flex w-full"}>
+                            <InputPanel onChange={(e) => this.handleInputChange(e)}/>
+                        </div>
                     </div>
                 }
             </Layout>
@@ -63,8 +85,32 @@ class Home extends PureComponent {
     }
 }
 
+const InputPanel = React.memo(function InputPanel(props) {
+    return <div className={"w-1/3 mr-auto ml-auto mt-12"}>
+                                <span className="w-full block ml-auto mr-auto align-middle text-center">
+                                    <img src={sanitizePublicPath("static/error_outline-24px.svg")} alt={""}
+                                         className={"inline mr-2 align-top"}/>
+                                    {props.text}
+                                    </span>
+        <label className="w-1/2 block ml-auto mr-auto mt-3">
+            <input type={"text"} onChange={props.onChange}
+                   className="form-input mt-1 block w-full" placeholder={props.placeholderText}/>
+        </label>
+    </div>
+});
+InputPanel.defaultProps = {
+    text: "Please enter your matriculation number / surname and choose a path.",
+    placeholderText: "it181508 / mayrhofer"
+};
+InputPanel.propTypes = {
+    text: PropTypes.string.isRequired,
+    placeholderText: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired
+};
+
 const OnboardingCard = React.memo(function OnboardingCard(props) {
-    return <ModeCard className={props.className} image={props.image} onClick={props.onClick} title={props.title}>
+    return <ModeCard className={props.className} image={props.image} onClick={props.onClick} title={props.title}
+                     enabled={props.enabled}>
         <ModeCardText>
             Start a data visualization onboarding tour consisting of 4 phases: Assessment,
             Onboarding, Task, Visualisation.
@@ -72,36 +118,42 @@ const OnboardingCard = React.memo(function OnboardingCard(props) {
     </ModeCard>;
 });
 OnboardingCard.defaultProps = {
-    title: "Option A",
+    title: "Path A",
     image: "static/visOnboarding_onboarding.png"
 };
 OnboardingCard.propTypes = {
     className: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     image: PropTypes.string.isRequired,
-    onClick: PropTypes.func.isRequired
+    onClick: PropTypes.func.isRequired,
+    enabled: PropTypes.bool.isRequired
 };
 
 const TaskCard = React.memo(function TaskCard(props) {
-    return <ModeCard className={props.className} image={props.image} onClick={props.onClick} title={props.title}>
+    return <ModeCard className={props.className} image={props.image} onClick={props.onClick} title={props.title}
+                     enabled={props.enabled}>
         <ModeCardText>
             Start without onboarding and navigate directly to the task.
         </ModeCardText>
     </ModeCard>;
 });
 TaskCard.defaultProps = {
-    title: "Option B",
+    title: "Path B",
     image: "static/visOnboarding_task.png"
 };
 TaskCard.propTypes = {
     className: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     image: PropTypes.string.isRequired,
-    onClick: PropTypes.func.isRequired
+    onClick: PropTypes.func.isRequired,
+    enabled: PropTypes.bool.isRequired
 };
 
 const ModeCard = React.memo(function ModeCard(props) {
-    let className = props.className + " bg-gray-100 flex hover:bg-blue-600 hover:text-white max-w-lg mt-auto mb-auto rounded overflow-hidden shadow-lg cursor-pointer p-2";
+    let className = props.className + " bg-gray-100 flex max-w-lg mt-auto mb-auto rounded overflow-hidden shadow-lg p-2";
+    let active = " hover:bg-blue-600 hover:text-white cursor-pointer";
+    let inActive = " cursor-not-allowed";
+    props.enabled ? className += active : className += inActive;
     return <div className={className} onClick={props.onClick}>
         <div className={""}>
             <img src={sanitizePublicPath(props.image)} alt=""/>
@@ -116,7 +168,8 @@ ModeCard.propTypes = {
     className: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     image: PropTypes.string.isRequired,
-    onClick: PropTypes.func.isRequired
+    onClick: PropTypes.func.isRequired,
+    enabled: PropTypes.bool.isRequired
 };
 
 const ModeCardText = React.memo(function ModeCardText(props) {
