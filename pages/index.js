2d72adf8b77a10c944fcf8815993cbafb827228e
Mathias Mayrhofer <it181508@fhstp.ac.at>
2020-03-04 10:48:37 +0100

    conditionalized home screen

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,35 @@
 import Layout from 'src/ui/Layout';
 import React, {PureComponent} from 'react';
-import {sanitizePublicPath} from "src/util/helpers";
 import Router from "next/router";
+import {sanitizePublicPath, withPageRouter} from "../src/util/helpers";
+import {Empty} from "../src/ui/components";
+import PropTypes from "prop-types";
 import {URL} from "../constants";
 
-export default class Home extends PureComponent {
+const OPTIONAL = "optional";
+const ONBOARDING = "onboarding";
+const TASK = "task";
+const MODES = [OPTIONAL, ONBOARDING, TASK];
+
+class Home extends PureComponent {
 
     constructor(props, context) {
         super(props, context);
+        this.state = {
+            loading: true,
+            mode: null
+        };
+    }
+
+    componentDidMount() {
+        let mode = MODES.includes(this.props.router.query.md) ? this.props.router.query.md : OPTIONAL;
+        this.setState({mode: mode}, () => {
+            this.setState({loading: false});
+        })
+    }
+
+    activeMode(identifier) {
+        return this.state.mode === identifier;
     }
 
     navigate(event, to) {
@@ -15,57 +37,92 @@ export default class Home extends PureComponent {
         Router.push(to).then(() => console.log("Redirecting: ", to));
     }
 
+    initCards() {
+        let cards = [];
+        if (this.activeMode(OPTIONAL)) {
+            cards.push(<OnboardingCard key={"card_1"} className={"mr-6 ml-auto"} onClick={(e) => this.navigate(e, URL.assessment)}/>);
+            cards.push(<TaskCard key={"card_2"} className={"ml-6 mr-auto"} onClick={(e) => this.navigate(e, URL.context)}/>)
+        } else if (this.activeMode(ONBOARDING)) {
+            cards.push(<OnboardingCard className={"m-auto"} onClick={(e) => this.navigate(e, URL.assessment)}/>);
+        } else if (this.activeMode(TASK)) {
+            cards.push(<TaskCard className={"m-auto"} onClick={(e) => this.navigate(e, URL.context)}/>);
+        }
+        return cards;
+    }
+
     render() {
         return (
             <Layout>
-                <strong className={"text-red-800"}>THIS PAGE IS UNDER CONSTRUCTION</strong>
-                <div id={"cards-flex-container"} className="flex flex-wrap">
-                    <div
-                        className="w-1/4 ml-auto hover:bg-green-400 max-w-sm rounded overflow-hidden shadow-lg mr-6 cursor-pointer"
-                        onClick={(e) => this.navigate(e, URL.assessment)}>
-                        <img className="w-full"
-                             src={sanitizePublicPath("static/person-holding-chart-and-bar-graph-669612.jpg")}
-                             alt="onboarding_img"/>
-                        <div className="px-6 py-4">
-                            <div className="font-bold text-xl mb-2">Onboarding</div>
-                            <p className="text-gray-700 text-base">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla!
-                                Maiores et perferendis eaque, exercitationem praesentium nihil.
-                            </p>
-                        </div>
-                        <div className="px-6 py-4">
-                            <span
-                                className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#onboarding</span>
-                            <span
-                                className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#data</span>
-                            <span
-                                className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#assessment</span>
-                        </div>
+                {this.state.loading ? <Empty/> :
+                    <div className={"flex flex-wrap w-full h-screen"}>
+                        {this.initCards()}
                     </div>
-                    <div
-                        className="w-1/4 mr-auto hover:bg-green-400 max-w-sm rounded overflow-hidden shadow-lg ml-6 cursor-pointer"
-                        onClick={(e) => this.navigate(e, URL.context)}>
-                        <img className="w-full" src={sanitizePublicPath("static/chart-close-up-data-desk-590022.jpg")}
-                             alt="visualistaion_img"/>
-                        <div className="px-6 py-4">
-                            <div className="font-bold text-xl mb-2">Visualisation</div>
-                            <p className="text-gray-700 text-base">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla!
-                                Maiores et perferendis eaque, exercitationem praesentium nihil.
-                            </p>
-                        </div>
-                        <div className="px-6 py-4">
-                            <span
-                                className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#visualisation</span>
-                            <span
-                                className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#task</span>
-                            <span
-                                className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#data</span>
-                        </div>
-                    </div>
-                </div>
+                }
             </Layout>
         );
     }
-
 }
+
+const OnboardingCard = React.memo(function OnboardingCard(props) {
+    return <ModeCard className={props.className} image={props.image} onClick={props.onClick} title={props.title}>
+        <ModeCardText>
+            Start a data visualization onboarding tour consisting of 4 phases: Assessment,
+            Onboarding, Task, Visualisation.
+        </ModeCardText>
+    </ModeCard>;
+});
+OnboardingCard.defaultProps = {
+    title: "Option A",
+    image: "static/visOnboarding_onboarding.png"
+};
+OnboardingCard.propTypes = {
+    className: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    onClick: PropTypes.func.isRequired
+};
+
+const TaskCard = React.memo(function TaskCard(props) {
+    return <ModeCard className={props.className} image={props.image} onClick={props.onClick} title={props.title}>
+        <ModeCardText>
+            Start without onboarding and navigate directly to the task.
+        </ModeCardText>
+    </ModeCard>;
+});
+TaskCard.defaultProps = {
+    title: "Option B",
+    image: "static/visOnboarding_task.png"
+};
+TaskCard.propTypes = {
+    className: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    onClick: PropTypes.func.isRequired
+};
+
+const ModeCard = React.memo(function ModeCard(props) {
+    let className = props.className + " bg-gray-100 flex hover:bg-blue-600 hover:text-white max-w-lg mt-auto mb-auto rounded overflow-hidden shadow-lg cursor-pointer p-2";
+    return <div className={className} onClick={props.onClick}>
+        <div className={""}>
+            <img src={sanitizePublicPath(props.image)} alt=""/>
+            <div className="px-6 py-4">
+                <h2 className={"mb-2"}>{props.title}</h2>
+                {props.children}
+            </div>
+        </div>
+    </div>
+});
+ModeCard.propTypes = {
+    className: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    onClick: PropTypes.func.isRequired
+};
+
+const ModeCardText = React.memo(function ModeCardText(props) {
+    return <p>
+        {props.children}
+    </p>
+});
+
+export default withPageRouter(Home)
