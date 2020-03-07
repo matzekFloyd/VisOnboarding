99eb9745eb3fdca328f390ff7c22884b072d83d3
Mathias Mayrhofer <it181508@fhstp.ac.at>
2020-03-07 12:27:32 +0100

    adds content for georg fischer fittings explanation in context.js

--- src/ui/context/Explanation.js
+++ src/ui/context/Explanation.js
@@ -8,7 +8,40 @@ export default class Explanation extends PureComponent {
         this.identifier = props.identifier;
     }
 
+    headline(text) {
+        return <ExplanationHeadline text={text}/>;
+    }
+
+    leftColumn(html) {
+        return <ExplanationLeftColumn html={html}/>;
+    }
+
+    rightColumn(html) {
+        return <ExplanationRightColumn html={html}/>;
+    }
+
 }
 Explanation.propTypes = {
     identifier: PropTypes.string.isRequired
+};
+
+const ExplanationHeadline = React.memo(function ExplanationHeadline(props) {
+    return <h2>{props.text}</h2>
+});
+ExplanationHeadline.propTypes = {
+    text: PropTypes.string.isRequired
+};
+
+const ExplanationLeftColumn = React.memo(function ExplanationLeftColumn(props) {
+    return props.html
+});
+ExplanationLeftColumn.propTypes = {
+    html: PropTypes.object.isRequired
+};
+
+const ExplanationRightColumn = React.memo(function ExplanationRightColumn(props) {
+    return props.html
+});
+ExplanationRightColumn.propTypes = {
+    html: PropTypes.object.isRequired
 };
--- src/ui/context/GeorgFischer.js
+++ src/ui/context/GeorgFischer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Explanation from "./Explanation";
+import {sanitizePublicPath} from "src/util/helpers";
 
 export default class GeorgFischer extends Explanation {
 
@@ -8,24 +9,56 @@ export default class GeorgFischer extends Explanation {
     }
 
     render() {
-        return <div className={"flex w-full"}>
-            <div className={"w-1/2 mr-6 "}>
-                <h2>Georg Fischer</h2>
-                <p>
-                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-                    eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-                    voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-                    clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-                    amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-                    nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-                    sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-                    rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-                    ipsum dolor sit amet.
-                </p>
-                <br/>
-            </div>
-            <div className={"w-1/2 ml-6 m-auto"}>
+        let leftCol = <div className={"left-col"}><p>
+            The Georg Fischer Fittings GmbH (+GF+) is located in Traisen, Lower Austria, and consists of
+            approximately 460 employees. On their industrial plant, 12.200 tons of <strong>pipe
+            fittings</strong> are produced every year.
+        </p>
+            <br/>
+            <p>
+                These pipe fittings are <strong>manufactured</strong> from <strong>malleable cast
+                iron</strong> and
+                can be used in a wide range of applications such as:
+            </p>
+            <ul className={"w-3/4 mr-auto ml-auto list-disc list-inside text-left mt-3"}>
+                <li>
+                    <i>sanitary, heating and gas installation</i>
+                </li>
+                <li>
+                    <i>cooling and air conditioning</i>
+                </li>
+                <li>
+                    <i>construction of industrial plants</i>
+                </li>
+                <li>
+                    <i>machine construction</i>
+                </li>
+                <li>
+                    <i>commercial vehicles construction</i>
+                </li>
+            </ul>
+            <br/>
+            <p>
+                The images on the right show two examples of their product.
+            </p>
+        </div>;
 
+        let rightCol = <div className={"flex w-full "}>
+            <img className={"w-1/2 m-auto"}
+                 src={sanitizePublicPath("static/context/malleable_cast_iron_fitting_1.jpg")}
+                 alt={"malleable_cast_iron_fitting_1"}/>
+            <img className={"w-1/2 m-auto"}
+                 src={sanitizePublicPath("static/context/malleable_cast_iron_fitting_2.jpg")}
+                 alt={"malleable_cast_iron_fitting_2"}/>
+        </div>;
+
+        return <div className={"flex w-full"}>
+            <div className={"w-1/2 mr-6 text-justify"}>
+                {this.headline("Georg Fischer Fittings GmbH")}
+                {this.leftColumn(leftCol)}
+            </div>
+            <div className={"w-1/2 ml-6 mr-auto mt-auto mb-auto"}>
+                {this.rightColumn(rightCol)}
             </div>
         </div>;
     }
