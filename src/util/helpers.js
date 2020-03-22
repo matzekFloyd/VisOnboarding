2e74ae98f1730312e8f48816372ce56e6c7dd3c9
Mathias Mayrhofer <it181508@fhstp.ac.at>
2020-03-22 12:02:38 +0100

    bug-fix: tooltips now show proper UTC date

--- src/util/helpers.js
+++ src/util/helpers.js
@@ -88,13 +88,13 @@ export function formatDateString(date, incYear = true, incMonth = true, incDay =
     let days_arr = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     let months_arr = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-    let year = date.getFullYear();
-    let month = months_arr[date.getMonth()];
-    let day = date.getDate();
-    let weekDay = days_arr[date.getDay()];
-    let hours = date.getHours();
-    let minutes = "0" + date.getMinutes();
-    let seconds = "0" + date.getSeconds();
+    let year = date.getUTCFullYear();
+    let month = months_arr[date.getUTCMonth()];
+    let day = date.getUTCDate();
+    let weekDay = days_arr[date.getUTCDay()];
+    let hours = date.getUTCHours();
+    let minutes = "0" + date.getUTCMinutes();
+    let seconds = "0" + date.getUTCSeconds();
     let dateString = "";
 
     if (incWeekDay) dateString += weekDay + " ";
